8846779b497799374342bd816aa68bfd39ca8ab7
sofiabasu05 <127398330+sofiabasu05@users.noreply.github.com>
2023-03-10 12:00:21 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@
 #my_fruit_list = my_fruit_list.set_index('Fruit')
 #streamlit.dataframe(my_fruit_list)
 
-# Let's put a pick list here so they can pick the fruit they want to include 
+#Let's put a pick list here so they can pick the fruit they want to include 
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
@@ -39,7 +39,7 @@
 #streamlit.text(fruityvice_response.json())
 
 
-# write your own comment -what does the next line do? 
+# rite your own comment -what does the next line do? 
 #fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 #streamlit.dataframe(fruityvice_normalized)
